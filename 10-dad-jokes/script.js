aee9e037c1cc560e724f71ead0c5a66eec7236b0
Yomar Miranda <mirandayomar@gmail.com>
2020-12-14 15:23:27 -0500

    feat: 10 - dad jokes using async/await.

--- 10-dad-jokes/script.js
+++ 10-dad-jokes/script.js
@@ -5,17 +5,15 @@ jokeBtn.addEventListener('click', generateJoke);
 
 generateJoke ();
 
-function generateJoke() {
+async function generateJoke() {
   const config = {
     headers: {
       'Accept': 'application/json'
     }
   }
 
-  fetch('https://icanhazdadjoke.com', config)
-  .then(resp => resp.json())
-  .then(data => {
-    jokeEl.innerHTML = data.joke;
-  })
+  const res = await fetch('https://icanhazdadjoke.com', config);
+  const data = await res.json();
+  jokeEl.innerHTML = data.joke;
 }
 
